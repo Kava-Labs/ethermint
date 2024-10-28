cf40f6a95ea14192e4196b9bd34407c0d2f54852
Vladyslav Budichenko <boodyvo@gmail.com>
2024-10-28 10:51:47 -0400

    wip: update tests and initialization

--- types/account.pb.go
+++ types/account.pb.go
@@ -12,7 +12,6 @@ import (
 	io "io"
 	math "math"
 	math_bits "math/bits"
-	"runtime/debug"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -69,7 +68,6 @@ var xxx_messageInfo_EthAccount proto.InternalMessageInfo
 
 func init() {
 	fmt.Println("init ethermint.types.v1.EthAccount")
-	debug.Stack()
 	proto.RegisterType((*EthAccount)(nil), "ethermint.types.v1.EthAccount")
 }
 
