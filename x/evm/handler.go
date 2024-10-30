46b887f2cc10479576c2fc2040d4e7d6fdb2c6ce
Vladyslav Budichenko <boodyvo@gmail.com>
2024-10-30 11:44:47 -0400

    wip: remove logging

--- x/evm/handler.go
+++ x/evm/handler.go
@@ -17,17 +17,15 @@ package evm
 
 import (
 	errorsmod "cosmossdk.io/errors"
+	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	errortypes "github.com/cosmos/cosmos-sdk/types/errors"
 
 	"github.com/evmos/ethermint/x/evm/types"
 )
 
-// Handler defines the core of the state transition function of an application.
-type Handler func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error)
-
 // NewHandler returns a handler for Ethermint type messages.
-func NewHandler(server types.MsgServer) Handler {
+func NewHandler(server types.MsgServer) baseapp.MsgServiceHandler {
 	return func(ctx sdk.Context, msg sdk.Msg) (result *sdk.Result, err error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 
